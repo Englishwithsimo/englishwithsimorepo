9b32f0f8a33ae024039f6283eb7c2f8ed4514e5a
Englishwithsimo <75704870+Englishwithsimo@users.noreply.github.com>
2021-11-18 13:04:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@
             function nextQuestion() {
                 submt = true;
                 $('#explanation').empty();
-                document.querySelector('#question').innerHtml = quiz[currentquestion]['question'];
+                $('#question').html(htmlEncode(quiz[currentquestion]['question']));
                 $('#pager').text('Question ' + Number(currentquestion + 1) + ' of ' + quiz.length);
                 if (quiz[currentquestion].hasOwnProperty('image') && quiz[currentquestion]['image'] != "") {
                     if ($('#question-image').length == 0) {
