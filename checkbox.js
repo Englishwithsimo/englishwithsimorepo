fd423328c7af13e430d0b40fdd4fc4d4c35bbb08
Englishwithsimo <75704870+Englishwithsimo@users.noreply.github.com>
2022-07-03 00:37:01 +0100

    Update checkbox.js

--- checkbox.js
+++ checkbox.js
@@ -1,19 +1,35 @@
- function ysCheckAnswers(form,correctAnswers) {
+function ysCheckAnswers(form, correctAnswers) {
       let inputs = [...form.elements];
       let checkboxInputs = inputs.filter((input) => input.type !== 'button');
+
+      // calculate number of correct answers
+      let numberOfCorrectAnswers = 0;
       checkboxInputs.forEach((input) => {
         if (input.checked && correctAnswers.some((answer) => answer === input.value)) {
-          input.nextElementSibling.style.backgroundColor = 'green';
+          numberOfCorrectAnswers++;
         }
-          if (!input.checked && !correctAnswers.some((answer) => answer === input.value)) {
-          input.nextElementSibling.style.backgroundColor = 'red';
+        if (input.checked && !correctAnswers.some((answer) => answer === input.value)) {
+          numberOfCorrectAnswers--;
         }
-                if (input.checked && !correctAnswers.some((answer) => answer === input.value)) {
-          input.nextElementSibling.style.backgroundColor = 'red';
-          input.nextElementSibling.style.textDecoration = 'underline';
-        }
-        if (!input.checked && correctAnswers.some((answer) => answer === input.value)) {
-          input.nextElementSibling.style.backgroundColor = 'red';
+      });
+
+      // are all the answers correct?
+      const areAllAnswersCorrect = numberOfCorrectAnswers === correctAnswers.length;
+
+      // check student answers
+      checkboxInputs.forEach((input) => {
+        if (areAllAnswersCorrect) {
+          if (input.checked && correctAnswers.some((answer) => answer === input.value)) {
+            input.nextElementSibling.style.backgroundColor = 'green';
+          } else {
+            input.nextElementSibling.style.backgroundColor = 'transparent';
+          }
+        } else {
+          if (input.checked && correctAnswers.some((answer) => answer === input.value)) {
+            input.nextElementSibling.style.backgroundColor = 'green';
+          } else {
+            input.nextElementSibling.style.backgroundColor = 'red';
+          }
         }
       });
     }
